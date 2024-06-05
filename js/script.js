427818e1ac5a51948699e81816cb0254278fa757
GencoMattia <gencomattia@hotmail.it>
2024-06-05 15:25:42 +0200

    JS: added mailList data; added getRandomEmail method; added created hook this.getRandomEmail

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            mailList: [
+
+            ],
         }
+    },
+
+    methods: {
+        getRandomEmail () {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(response => {
+                const result = response.data;
+                console.log(result)
+            });
+        }
+    },
+
+    created() {
+        this.getRandomEmail();
     }
 }).mount('#app')
